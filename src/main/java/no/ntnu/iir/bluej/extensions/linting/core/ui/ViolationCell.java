7323b7025a1f764517b564ca5ca8ccf954fd2b88
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-02-01 17:59:09 +0100

    feat: bind WebView to parent after reinstantiating + codestyle fixes

--- src/main/java/no/ntnu/iir/bluej/extensions/linting/core/ui/ViolationCell.java
+++ src/main/java/no/ntnu/iir/bluej/extensions/linting/core/ui/ViolationCell.java
@@ -14,7 +14,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
-import javafx.scene.web.WebView;
 import no.ntnu.iir.bluej.extensions.linting.core.editor.EditorNotifier;
 import no.ntnu.iir.bluej.extensions.linting.core.violations.RuleDefinition;
 import no.ntnu.iir.bluej.extensions.linting.core.violations.Violation;
